b84ce923841c7099f81e6576f2140d07f4e84411
Andreas Longo <git@andreaslongo.com>
2024-08-25 14:00:38 +0200

    Update

--- container/run.sh
+++ container/run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Starts a new container based on the DEV image or attaches to an already
+# running DEV container.
+#
+# Programs run as non-root-user inside the container for security reasons.
+#
+# The container mounts the project directory via bind-mount. Only a single
+# container (and the host) can access the project files (`:Z` uppercase).
+#
+# The container uses named volumes for persisting cache files to speed up the
+# development process. Multiple containers can access the cache volumes
+# simultaneously (`:z` lowercase).
+
 readonly local script_dir=$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )
 readonly local parent_dir=$(dirname ${script_dir})
 
@@ -7,6 +19,9 @@ readonly local parent_dir=$(dirname ${script_dir})
 readonly local uid=1000
 readonly local gid=1000
 
+# TODO:
+#  --volume "rust_cargo:/usr/local/cargo:Z,rw" \
+#  --volume "rust_rustup:/usr/local/rustup:Z,rw" \
 # Use --publish <host_port>:<container_port> to enable networking.
 # Use --rm to remove the container after exit.
 # Use --env-file "${parent_dir}/.env" to read environment variables from a file.
@@ -18,8 +33,10 @@ podman container start --attach --interactive "$(basename ${parent_dir})" 2>/dev
     --tty \
     --user ${uid}:${gid} \
     --userns keep-id:uid=${uid},gid=${gid} \
+    --volume "${HOME}/.cache/huggingface:/home/appuser/.cache/huggingface:z,rw" \
+    --volume "${HOME}/.cache/pre-commit:/home/appuser/.cache/pre-commit:z,rw" \
+    --volume "${HOME}/.cache/rattler:/home/appuser/.cache/rattler:z,rw" \
+    --volume "${HOME}/.pixi:/home/appuser/.pixi:z,rw" \
     --volume "${parent_dir}:/home/appuser/$(basename ${parent_dir}):Z,rw" \
-    --volume "rust_cargo:/usr/local/cargo:Z,rw" \
-    --volume "rust_rustup:/usr/local/rustup:Z,rw" \
     --workdir "/home/appuser/$(basename ${parent_dir})" \
         localhost/"$(basename ${parent_dir})":latest
